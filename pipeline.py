ed4121a57f6906cce3c92a0e0439ea11237cb60f
Shirasoki <maxleon_lappalainen@hotmail.com>
2025-04-28 16:39:30 +0200

    Working on dlt

---
+++ pipeline.py
@@ -0,0 +1,9 @@
+import dlt
+import Test_dlt
+pipeline = dlt.pipeline(
+    pipeline_name = "jobtech_pipeline",
+    destination= "duckdb",
+    dataset_name ="jobs_dataset"
+)
+info = pipeline.run(jobtech_source())
+print(info)
