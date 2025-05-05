27ca7ce3d2dd4eb14947f5899422f5c42a88387f
Elvira L <elvira.liljestrand@hotmail.com>
2025-05-05 17:20:27 +0200

    Put test chart to run from main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import duckdb
 import pandas as pd
-from Dashboard.dashboard import set_bg_pic
-from dbt_groupp.models.mart import mart_pedagogik, mart_säker_bevak, mart_transp_ditr_lager
-con = duckdb.connect('jobs.duckdb')
+#from Dashboard.dashboard import set_bg_pic
+from Dashboard.charts import ads_per_occupation
 
 #set_bg_pic('./Media/forest.png') 
+ads_per_occupation()
