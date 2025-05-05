3528a07276e15b1758f474fc32499f855fa92dd5
Elvira L <elvira.liljestrand@hotmail.com>
2025-05-05 18:57:43 +0200

    Remade the mart schemas

--- dbt_groupp/models/mart/mart_pedagogik.sql
+++ dbt_groupp/models/mart/mart_pedagogik.sql
@@ -1,14 +1,15 @@
 -- mart schema for the occupation field "pedagogik"
 {{ config(materialized='view', schema='occupation') }}
 
+-- Join fct_job_ads with dim_employer to create a mart view for Transport och lager occupation
 SELECT
-    job_id,
-    occupation_id,
-    employer_id,
-    auxilliary_attributes_id,
-    region,
-    number_of_vacancies,
-    relevance,
-    deadline
-FROM {{ ref('fct_job_ads') }}
-WHERE occupation_category = 'Pedagogik'
+    f.job_id,
+    f.employer_id,
+    d.employer_name,
+    f.number_of_vacancies,
+    d.region,
+    d.municipality,
+    d.country
+FROM {{ ref('fct_job_ads') }} f
+LEFT JOIN refined.dim_employer d ON f.employer_id = d.employer_id
+WHERE f.occupation_category = 'Pedagogik'
