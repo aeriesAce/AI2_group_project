bfdfbd4e099e2e21b9fed6fcbb4e2efd47e6f97f
Elvira L <elvira.liljestrand@hotmail.com>
2025-05-09 08:45:08 +0200

    Moved files to folder Tests

--- d_test1.py
+++
@@ -1,7 +0,0 @@
-from Dashboard.ui import set_sidebar, sidebar_choices, set_bg_pic
-
-df = None
-# using None just to try out the dashboard
-set_bg_pic('./Media/test1.webp') 
-choicr, detail = set_sidebar(df)
-sidebar_choices(detail)
