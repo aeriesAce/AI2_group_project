0c71d0ca39c787ccf63860d949e530ad25094d7a
Kalligall <168739152+Kalligall@users.noreply.github.com>
2025-05-12 14:33:07 +0200

    fixed dim tables and fct_job_ads to not duplicate adds into warehouse.

--- dbt_groupp/models/dim/dim_occupation.sql
+++ dbt_groupp/models/dim/dim_occupation.sql
@@ -7,7 +7,7 @@ WITH source AS(
     FROM {{ ref('stg_jobs') }}
 )
 SELECT
-    {{ dbt_utils.generate_surrogate_key(['occupation_group', 'occupation_category']) }} AS occupation_id, --surrogat nyckel då vi har transformerat den datan i staging.
+    {{ dbt_utils.generate_surrogate_key(['occupation_group', 'occupation_category', 'occupation_label']) }} AS occupation_id, --surrogat nyckel då vi har transformerat den datan i staging.
     occupation_group,
     occupation_category,
     occupation_label
