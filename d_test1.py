3a88d249464f8c55b01b570929bc175237eb2334
Elvira L <elvira.liljestrand@hotmail.com>
2025-05-14 10:44:10 +0200

    Fixed mart-schema names and added src

--- d_test1.py
+++ d_test1.py
@@ -1,7 +1,7 @@
-from Dashboard.ui import set_bg_pic, set_sidebar, sidebar_choices
+from Dashboard.ui import set_sidebar, sidebar_choices
 
 df = None
 # using None just to try out the dashboard
-set_bg_pic('./Media/test1.webp') 
-choicr, detail = set_sidebar(df)
+ 
+choice, detail = set_sidebar(df)
 sidebar_choices(detail)
