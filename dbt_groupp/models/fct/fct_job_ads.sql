b068aa9251d05a115719be23ece9a860d1792280
Kalligall <168739152+Kalligall@users.noreply.github.com>
2025-05-21 19:22:14 +0200

    fixed the errors revolving a dbt build, also removed the unique checks on dim_id's cuz brainfart when making them

--- dbt_groupp/models/fct/fct_job_ads.sql
+++ dbt_groupp/models/fct/fct_job_ads.sql
@@ -1,7 +1,7 @@
 SELECT
     {{ dbt_utils.generate_surrogate_key(['occupation_group', 'occupation_label', 'occupation_category']) }} AS occupation_id,
     {{ dbt_utils.generate_surrogate_key(['job_id', 'headline']) }} AS job_details_id,
-    {{ dbt_utils.generate_surrogate_key(['employer_name', 'municipality']) }} AS employer_id,
+    {{ dbt_utils.generate_surrogate_key(['employer_workplace', 'municipality']) }} AS employer_id,
     {{ dbt_utils.generate_surrogate_key(['experience_required', 'driving_license', 'own_car']) }} AS auxilliary_id,
     COALESCE(CAST(number_of_vacancies AS INTEGER), 1) AS vacancies,
     relevance,
