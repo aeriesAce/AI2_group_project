344a4605e3e2b1924f99053e49991311e19dc987
Kalligall <168739152+Kalligall@users.noreply.github.com>
2025-05-31 11:22:56 +0200

    fixed the issue where dagster was creating a database in the wrong place, aswell as added a check to only run the pipeline if its from main

--- Data/orchestration/definitions.py
+++ Data/orchestration/definitions.py
@@ -7,11 +7,14 @@ import dlt
 from pathlib import Path
 
 import sys
-sys.path.insert(0, str(Path(__file__).parents[1] / '../Data'))
+ROOT_DIR = Path(__file__).resolve().parents[2]
+DATA_DIR = ROOT_DIR / "Data"
+sys.path.insert(0, str(DATA_DIR))
+
 from pipeline import jobtech_source
 from dlt.destinations import duckdb
 
-db_path = str(Path(__file__).parents[0] /"jobs.duckdb")
+db_path = str(ROOT_DIR /"jobs.duckdb")
 
 #dlt asset
 dlt_resource = DagsterDltResource()
@@ -28,7 +31,7 @@ def dlt_load(context: dg.AssetExecutionContext, dlt: DagsterDltResource):
     yield from dlt.run(context=context)
 
 #dbt asset
-dbt_project_directory = Path(__file__).parents[1].parent / "dbt_groupp"
+dbt_project_directory = ROOT_DIR / "dbt_groupp"
 
 profiles_dir = Path.home()/".dbt"
 
@@ -44,7 +47,10 @@ dbt_project.prepare_if_dev()
 def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
 #job
-job_dlt = dg.define_asset_job("job_dlt", selection=dg.AssetSelection.assets("dlt_jobtech_source_jobs"))
+job_dlt = dg.define_asset_job("job_dlt", selection=dg.AssetSelection.assets("dlt_jobtech_source_jobs").downstream())
+
+## we dont really need this line of code anymore, but saving it incase we want to add more pipelines or a full on automation for job_dbt.
+## because this is being handled by job_dlt with a downstream now.
 job_dbt = dg.define_asset_job("job_dbt", selection=dg.AssetSelection.assets('stg_jobs').downstream())
 
 # schedule that runs job_dlt every day at 8:17 UTC
--- Data/pipeline.py
+++ Data/pipeline.py
@@ -5,25 +5,28 @@ from get_data import jobtech_source
 DB_path = "jobs.duckdb"
 TABLE_NAME = "staging.jobs"
 
-con = duckdb.connect(DB_path)
-try: 
-    before_count = con.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}").fetchone()[0]
-except duckdb.CatalogException:
-    before_count = 0
+def run_pipeline():
+    con = duckdb.connect(DB_path)
+    try: 
+        before_count = con.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}").fetchone()[0]
+    except duckdb.CatalogException:
+        before_count = 0
 
-pipeline = dlt.pipeline(
-    pipeline_name = "jobs",
-    destination= "duckdb",
-    dataset_name ="staging"
-)
-info = pipeline.run(jobtech_source())
-print(info)
+    pipeline = dlt.pipeline(
+        pipeline_name = "jobs",
+        destination= "duckdb",
+        dataset_name ="staging"
+    )
+    info = pipeline.run(jobtech_source())
+    print(info)
 
-after_count = con.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}").fetchone()[0]
-new_rows = after_count - before_count
+    after_count = con.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}").fetchone()[0]
+    new_rows = after_count - before_count
 
-print(f"new adds added: {new_rows}")
+    print(f"new adds added: {new_rows}")
 
+if __name__ == "__main__":
+    run_pipeline()
 ## test to see how much is being loaded from the pipeline after it has run.
 ##con = duckdb.connect("DB_path")
 ##result = con.execute(f"SELECT COUNT(*) FROM {TABLE_NAME}").fetchone()
